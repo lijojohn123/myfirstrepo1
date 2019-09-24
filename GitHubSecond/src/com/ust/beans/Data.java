1217276dbb2e682f07e836ddf5d235b572e68dfd
dev20 <dev20@dev20-desktop>
2019-09-24 15:24:09 +0530

    first

---
+++ GitHubSecond/src/com/ust/beans/Data.java
@@ -0,0 +1,11 @@
+package com.ust.beans;
+
+public class Data {
+
+	public void display() {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Welcome to Main!!");
+	}
+
+}
---
+++ GitHubSecond/src/com/ust/beans/Main.java
@@ -0,0 +1,19 @@
+package com.ust.beans;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Data objSalesData = new Data();
+		System.out.println("Welcome to Sales App v2019");
+		displayGreeting();
+		objSalesData.display();
+
+	}
+
+	private static void displayGreeting() {
+		System.out.println("Welcome to Sales App");
+
+	}
+
+}
